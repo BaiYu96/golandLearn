8658a4478224ac20c194458fe27490591accf6d3
baiyu <zhh_baiyu@163.com>
2019-10-17 18:09:58 +0800

    the floder is my godoc study notes

---
+++ learn doc/test/a.go
@@ -0,0 +1,6 @@
+// 这个是a.go的包名的描述
+//
+// At 20191017 by Baiyu
+//
+// ---a.go end---
+package test
---
+++ learn doc/test/doc.go
@@ -0,0 +1,12 @@
+// 这个是在doc.go 文件中的内容
+//
+// 在Go的源代码中
+//
+//在Mac系统中
+//
+//在类Unix系统中
+//
+//在Windows系统中
+//
+// ---doc.go end---
+package test
